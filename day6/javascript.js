ee2e2d47926f21a4e04c051ecb85f7f04dadf3d2
TuyishimireProvidence <Providencetuyishimire555@gmail.com>
2023-11-28 16:44:23 +0200

    update

---
+++ day6/javascript.js
@@ -0,0 +1,10 @@
+//A brief word about functions
+console.log("let's  begin:");  //  ->  let's  begin:
+console.log("Hello");  //  ->  Hello
+console.log("World");  //  ->  World
+console.log("and  again:");  //  ->  and  again:
+console.log("Hello");  //  ->  Hello
+console.log("World");  //  ->  World
+console.log("and  once  more:");  //  ->  and  once  more:
+console.log("Hello");  //  ->  Hello
+console.log("World");  //  ->  World
